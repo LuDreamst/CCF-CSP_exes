1968105fae97b617ea45fcffb84e8b287efae229
LuDreamst <53106447+LuDreamst@users.noreply.github.com>
2024-01-15 17:36:31 +0800

    树上搜索

--- 202312-3.py
+++ 202312-3.py
@@ -1,22 +1,17 @@
 # 树上搜索
 n,m = map(int,input().split(" "))
-number = list(range(1, n+1, 1))
-number_reverse = number[::-1]
+number_reverse = list(range(n, 0, -1))
 
-number2 = list(range(2, n+1, 1))
-number2_reverse = number2[::-1]
+number2_reverse = list(range(n, 1, -1))
 
 rate = list(map(int,input().split(" ")))
-rate_reverse = rate[::-1]
 
-dict_number_rate = dict(zip(number_reverse,rate_reverse))
+dict_number_rate = dict(zip(number_reverse,reversed(rate))) # 从大到小排列
 
 number_upper = list(map(int,input().split(" ")))
-number_upper_reverse = number_upper[::-1]
-dict_number_upper = dict(zip(number2_reverse,number_upper_reverse))
+dict_number_upper = dict(zip(number2_reverse,reversed(number_upper))) # 从大到小排列
 
 number_test = []
-# number_test.append(int(input().split("")))
 for i in range(m):
     number_test.append(int(input()))
 
@@ -25,7 +20,7 @@ def number_race(dict_number_upper):
     for i in number_reverse:
         dict_number_race[i] = [i]
     for value in dict_number_race.values():
-        for key2 in dict_number_upper.keys():
+        for key2 in dict_number_upper:
             if value[-1] == key2 and value[-1] != 1:
                 value.append(dict_number_upper[key2])
             elif value == key2:
@@ -37,15 +32,15 @@ def w_delta_calc(dict_number_rate,dict_number_upper,number_reverse):
     temp_dict_number_upper = dict_number_upper.copy()
     w_delta = []
     sum = 0
-    for key in temp_dict_number_rate.keys():
+    for key in temp_dict_number_rate:
         sum += temp_dict_number_rate[key]
-    for key in temp_dict_number_rate.keys():
+    for key in temp_dict_number_rate:
         if key == 1:
             w_delta.append(abs(sum))
         elif not any(key == value for value in temp_dict_number_upper.values()):
             w_delta.append(abs(sum - temp_dict_number_rate[key] - temp_dict_number_rate[key]))
         else:
-            for key2 in temp_dict_number_upper.keys():
+            for key2 in temp_dict_number_upper:
                 if key == temp_dict_number_upper[key2]:
                     temp_dict_number_rate[key] += temp_dict_number_rate[key2]
             w_delta.append(abs(sum - temp_dict_number_rate[key] - temp_dict_number_rate[key]))
@@ -53,7 +48,7 @@ def w_delta_calc(dict_number_rate,dict_number_upper,number_reverse):
 
 def min_dict_w_delta_key(dict_w_delta):
     min_w_delta = min(dict_w_delta.values())
-    for key in dict_w_delta.keys():
+    for key in dict_w_delta:
         if dict_w_delta[key] == min_w_delta:
             min_key = key
     return min_key
@@ -71,7 +66,7 @@ for i in number_test:
         min_key = min_dict_w_delta_key(dict_w_delta)
         if min_key not in dict_number_race[i]:# 旁支，删除该类别及其后代类别
             list_number.append(min_key) #只记录min_key类别，其后代类别只删除，不记录
-            for key in list(temp_dict_number_race2.keys()):
+            for key in list(temp_dict_number_race2):
                 if min_key in temp_dict_number_race2[key]:
                     del temp_dict_number_rate2[key]
                     del temp_dict_number_race2[key]
@@ -81,7 +76,7 @@ for i in number_test:
         elif min_key in dict_number_race[i]: # min_key处于同支，进一步讨论
             list_number.append(min_key)
             if min_key == i:# 找到目标类别，仅保留该类别及其后代类别
-                for key in list(temp_dict_number_race2.keys()):
+                for key in list(temp_dict_number_race2):
                     if min_key not in temp_dict_number_race2[key]:
                         del temp_dict_number_rate2[key]
                         del temp_dict_number_race2[key]
